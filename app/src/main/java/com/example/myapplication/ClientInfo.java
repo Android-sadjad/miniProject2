c87b040c2f8c88a0c6ca5f9b2804a5d254e2152e
Mohammad Yazdi <mohammadyt1379@gmail.com>
2021-05-19 06:29:58 +0430

    create MainActivity code

---
+++ app/src/main/java/com/example/myapplication/ClientInfo.java
@@ -0,0 +1,57 @@
+package com.example.myapplication;
+
+public class ClientInfo {
+
+    String ImageAddress;
+    String name;
+    String skill;
+    String job;
+    String phoneNumber;
+    String whereSeen;
+    String description;
+
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getImageAddress() {
+        return ImageAddress;
+    }
+    public void setImageAddress(String imageAddress) {
+        ImageAddress = imageAddress;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSkill() {
+        return skill;
+    }
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+    public String getJob() {
+        return job;
+    }
+    public void setJob(String job) {
+        this.job = job;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getWhereSeen() {
+        return whereSeen;
+    }
+    public void setWhereSeen(String whereSeen) {
+        this.whereSeen = whereSeen;
+    }
+
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,112 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView clientImage;
+    EditText etName;
+    EditText etSkill;
+    EditText etJob;
+    EditText etPhoneNumber;
+    EditText etWhereSeen;
+    EditText etDescription;
+    Button btnRegister;
+
+    ClientInfo clientInfo;
+    Animation buttonAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+        init();
+        config();
+
     }
+
+    private void findViews() {
+
+        clientImage=findViewById(R.id.client_image);
+
+        etName=findViewById(R.id.et_name);
+        etSkill=findViewById(R.id.et_skill);
+        etJob=findViewById(R.id.et_job);
+        etPhoneNumber=findViewById(R.id.et_phone_number);
+        etWhereSeen=findViewById(R.id.et_where_seen);
+        etDescription=findViewById(R.id.et_description);
+
+        btnRegister=findViewById(R.id.btn_register);
+
+
+    }
+
+    private void init() {
+
+        clientInfo=new ClientInfo();
+        buttonAnimation= AnimationUtils.loadAnimation(this,R.anim.button_animation);
+
+    }
+
+    private void config() {
+
+        btnRegister.setOnClickListener(v -> {
+
+
+            btnRegister.startAnimation(buttonAnimation);
+            getDateFromClient(clientInfo);
+
+        });
+
+    }
+
+    private void getDateFromClient(ClientInfo clientInfo) {
+
+        if(evaluate()){
+
+         //   clientInfo.ImageAddress=clientImage.getResources().toString();
+
+            clientInfo.name=etName.getText().toString().trim();
+            clientInfo.skill=etSkill.getText().toString().trim();
+            clientInfo.job=etJob.getText().toString().trim();
+            clientInfo.phoneNumber=etPhoneNumber.getText().toString().trim();
+            clientInfo.whereSeen=etWhereSeen.getText().toString().trim();
+            clientInfo.description=etDescription.getText().toString().trim();
+
+            Toast.makeText(this, "اطلاعات با موفقیت ذخیره شد", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    private boolean evaluate() {
+
+        if (etName.getText().length()==0
+        ||etSkill.getText().length()==0
+        ||etJob.getText().length()==0
+        ||etPhoneNumber.getText().length()==0
+        ||etWhereSeen.getText().length()==0
+        ||etDescription.getText().length()==0){
+
+            showError();
+            return false;
+        }
+
+        else
+        return  true;
+    }
+
+    private void showError() {
+
+        Toast.makeText(this, "لطفا همه فیلد ها را وارد کنید", Toast.LENGTH_SHORT).show();
+    }
+
 }
